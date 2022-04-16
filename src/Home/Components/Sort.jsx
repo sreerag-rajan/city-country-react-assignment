924d6db1e049a2f8c61f796dd4eb96b10d09cc70
sreerag-rajan <85482640+sreerag-rajan@users.noreply.github.com>
2022-04-16 21:16:31 +0530

    fixed sorting

--- src/Home/Components/Sort.jsx
+++ src/Home/Components/Sort.jsx
@@ -8,12 +8,15 @@ export const Sort = ()=>{
 
     const handleChange = (e)=>{
         const {value} = e.target
+        console.log(value)
         let x;
         if(value==="popasc"){
-            x = cities.sort((a,b)=>a.population-b.population)            
+            x = cities.sort((a,b)=>a.population-b.population)
+            console.log(x)   
         }
         else{
             x = cities.sort((a,b)=>b.population-a.population)  
+            console.log(x)
         }
         dispatch(getCities(x));
    
@@ -22,6 +25,7 @@ export const Sort = ()=>{
     return(
         <div>
             <select onChange={handleChange}>
+                <option value="">Select Sort</option>
                 <option value="popasc">Population Ascending</option>                
                 <option value="popdesc">Population Descending</option>
             </select>
